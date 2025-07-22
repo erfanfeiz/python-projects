18576617d83cfdd98fe4e28dae2509b9788c2aef
a.feiz <amirfeiz7@gmail.com>
2025-07-22 17:11:51 +0330

    Patterns: completed another 6 patterns

--- patterns.py
+++ patterns.py
@@ -22,5 +22,96 @@ def pattern2(n):
     for i in range(0, n, 1):
         for j in range(0, i + 1, 1):
             print('*', end='')
-        print("")    
-pattern2(5)
+        print("")
+'''
+pattern 3
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+'''
+def pattern3(n):
+    for i in range(1, n + 1, 1):
+        for j in range(1, i  + 1, 1):
+            print(f"{j} ", end='')
+        print("\n")    
+
+'''
+pattern 4 
+1
+22
+333
+4444
+55555
+'''
+def pattern4(n):
+    for i in range(1, n + 1, 1):
+        for j in range(1, i + 1, 1):
+            print (f"{i} ", end = "")
+        print("\n")
+
+'''
+pattern 5
+*****
+****
+***
+**
+*
+'''
+def pattern5(n):
+    for i in range (0, n + 1, 1):
+        for j in range (1, n + 1 - i, 1):
+            print("*", end='')
+        print("\n")
+'''
+pattern 6
+12345
+1234
+123
+12
+1
+'''
+def pattern6(n):
+    for i in range (0, n + 1, 1):
+        for j in range (1, n + 1 - i, 1):
+            print(f"{j} ", end='')
+        print("\n")
+
+'''
+pattern 7
+    *
+   ***
+  *****
+ *******
+*********
+'''
+def pattern7(n):
+    for i in range (0, n , 1):
+        for x in range(n - 1 - i, 0, -1):
+                print(" ", end='')
+        for j in range(0, 2 * i + 1 , 1):
+            print("*", end='')
+        for x in range(n - 1 - i, 0, -1):
+            print(" ", end='')
+        print("\n")
+
+'''
+pattern 8
+*********
+ *******
+  *****
+   ***
+    *
+'''
+def pattern8(n):
+    for i in range (n, 0 , -1):
+        for x in range(i , n , 1):
+                print(" ", end='')
+        for j in range(0, 2 * i - 1 , 1):
+            print("*", end='')
+    
+        print("\n")
+
+
+pattern8(5)
