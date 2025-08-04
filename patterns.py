3d0fa36e53c5aafba11e78ad908d31c7f447e684
a.feiz <amirfeiz7@gmail.com>
2025-08-04 15:05:13 +0330

    Added patterns 12, 13, 14

--- patterns.py
+++ patterns.py
@@ -155,7 +155,7 @@ def pattern10(n):
             print("*", end='')
         print("\n")
 '''
-pattern 11
+pattern 11 ** this was challenging**
 1
 0 1
 1 0 1
@@ -173,4 +173,54 @@ def pattern11(n):
             print( f"{start} ", end = '')
             start = 1 - start 
         print("\n")
-pattern11(5)
+
+'''
+pattern 12
+
+1      1
+12    21
+123  321
+12344321
+
+'''
+def pattern12(n):
+    for i in range(1, n + 1, 1):
+        spaces = n * 2 - i * 2
+        for j in range(1, i + 1, 1):
+            print(j, end='')
+        for x in range(spaces):
+            print(" ", end = '')
+        for j in range(i, 0, -1):
+            print(j, end='')
+        
+        print("\n")        
+'''
+pattern 13
+
+1
+2 3
+4 5 6
+7 8 9 10 
+11 12 13 14 15
+'''
+def pattern13(n):
+    number = 1
+    for i in range(1, n + 1, 1):
+        for j in range(0, i , 1):
+            print(f"{number} ", end='')
+            number+=1
+        print("\n")
+'''
+pattern 14
+A
+AB
+ABC
+ABCD
+ABCDE
+'''
+def pattern14(n):
+    for i in range(0, n, 1):
+        for j in range(0, i  + 1 , 1):
+            print(chr(65 + j), end='')
+        print("\n")
+pattern14(5)
